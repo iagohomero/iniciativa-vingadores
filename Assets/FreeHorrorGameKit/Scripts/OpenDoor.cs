27ccd8b596530f7a477d6a2cb526118ddff1bcde
Iago Homero <iagohomero@gmail.com>
2019-09-17 22:31:58 -0300

    first commit

---
+++ Assets/FreeHorrorGameKit/Scripts/OpenDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public Animator _animator;
+
+    private bool _colidindo;
+    private bool _portaAbera = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _colidindo)
+        {
+            _portaAbera = true;
+            this.gameObject.SetActive(false);
+            this.gameObject.GetComponent<PlayerBehaviour>().pickUpUI.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            _colidindo = true;
+            _col.gameObject.GetComponent<PlayerBehaviour>().pickUpUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            if (_portaAbera)
+            {
+                this.gameObject.SetActive(true);
+            }
+
+            _colidindo = false;
+        }
+    }
+}
